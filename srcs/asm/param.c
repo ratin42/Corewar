24c9047bb5e023c0749932ebe6a928bd7fb65131
Raphael TIN <ratin@e1r3p8.42.fr>
2019-07-28 19:47:22 +0200

    neg

--- srcs/asm/param.c
+++ srcs/asm/param.c
@@ -6,7 +6,7 @@
 /*   By: ratin <ratin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 23:43:44 by ratin             #+#    #+#             */
-/*   Updated: 2019/07/28 19:36:39 by ratin            ###   ########.fr       */
+/*   Updated: 2019/07/28 19:38:12 by ratin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ void			fill_params(t_asm *asmbly, char *str, int line)
 
 	i = 0;
 	instru = find_instru(asmbly, line);
-	printf("str at begin = %s\n\n", &str[i]);
 	while (str[i])
 	{
 		y = 0;
@@ -61,9 +60,7 @@ void			fill_params(t_asm *asmbly, char *str, int line)
 			i++;
 		if ((y = get_comma(str, i)) == -1)
 			return ;
-		printf("str de i = %s str de y = %s\n", &str[i], &str[y]);
 		param = ft_strsub(str, i, y);
-		printf("param = |%s|\n\n", param);
 		add_param(instru, line, param);
 		free(param);
 		i += y;
--- zork.s
+++ zork.s
@@ -2,7 +2,7 @@
 .comment "hey"
 
 l2: 	sti r1,%:live,%1
-		and r1, %0, r1
+		and r1, -215, r1
 
 live:	live %1
 		zjmp %:live
