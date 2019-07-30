b22cd5f546fa598d6daa0e0ccbdab479936e0d9f
Syzhang16 <syzhang@student.42.fr>
2019-07-30 16:59:12 +0200

    structures added

--- vm/includes/vm.h
+++ vm/includes/vm.h
@@ -1,20 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzhang <syzhang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 22:35:47 by syzhang           #+#    #+#             */
+/*   Updated: 2019/07/30 16:57:57 by syzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef VM_H
 # define VM_H
 
-#include "../libft/includes/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <locale.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include "op.h"
+# include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+//#include "../../includes/op.h"
+//#include "../libft/includes/libft.h"
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include "../../includes/op.h"
+# include <stdio.h>
 
-#include <stdio.h>
+# define DEBUG 1
 
-#define DEBUG 1
+typedef struct			s_type
+{
+	int					param1;
+	int					param2;
+	int					param3;
+}                       t_type;
+
+typedef struct			s_op
+{
+	char				*name;
+	int					nbr_of_param;
+	t_type	        	type_of_param;
+	int					opcode;
+	int					nbr_of_cycle;
+	char				*description;
+	unsigned int		coding_opcode;
+	unsigned int		direct_size;
+}						t_op;
+
+typedef struct          s_process
+{
+    int                 reg[REG_NUMBER + 1];
+    unsigned int        pc;
+    unsigned int        carry;
+    unsigned int        live;
+    t_op                instruction;
+    struct s_process    *next;
+}                       t_process;
+
+typedef struct          s_champion
+{
+    char                *name;
+    char                *comment;
+    int                 id;
+    unsigned int        magic;
+    unsigned int        prog_size;
+}                       t_champion;
+
+typedef struct 			s_corewar
+{
+	unsigned char		ram[MEM_SIZE];
+	unsigned int		head;
+	unsigned int		tail;
+	unsigned int 		ram_full;
+	int					count;
+}						t_corewar;
 
 typedef	struct		s_instru
 {
-	//
+//
 }					t_instru;
 
 typedef	struct		s_process
---
+++ vm/srcs/get_type.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_type.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syzhang <syzhang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/29 23:01:45 by syzhang           #+#    #+#             */
+/*   Updated: 2019/07/30 15:54:12 by syzhang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm.h"
+
+int		register_range(int value, int min, int max)
+{
+	if (value >= min && value <= max)
+		return (0);
+	return (1);
+}
+
+int is_register(unsigned char octet)
+{
+    if (octet == 0b01000000)
+        return (0);
+    return (1);
+}
+
+int is_indirect(int octet)
+{
+    if (octet == 0b11000000)
+        return (0);
+    return (1);
+}
+
+int is_direct(int octet)
+{
+    if (octet == 0b10000000)
+        return (0);
+    return (1);
+}
+
+// int main()
+// {
+//     int byte = 0b01000000;
+//     printf("ret = %d", is_register(byte));
+//     return 0;
+// }
