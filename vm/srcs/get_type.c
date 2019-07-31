9cb5250b03a376bcc246a14b00f0c9cf203d83f2
Syzhang16 <syzhang@student.42.fr>
2019-07-31 18:32:12 +0200

    better get_type

--- vm/srcs/get_type.c
+++ vm/srcs/get_type.c
@@ -6,7 +6,7 @@
 /*   By: syzhang <syzhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 23:01:45 by syzhang           #+#    #+#             */
-/*   Updated: 2019/07/31 16:02:24 by syzhang          ###   ########.fr       */
+/*   Updated: 2019/07/31 18:31:20 by syzhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ int		register_range(int value, int min, int max)
 
 int is_register(unsigned char octet)
 {
-    if (octet == 0b01000000)
+    if (octet == (REG_CODE << 6))
         return (T_REG);
     return (1);
 }
 
 int is_indirect(int octet)
 {
-    if (octet == 0b11000000)
+    if (octet == (IND_CODE << 6))
         return (T_IND);
     return (1);
 }
 
 int is_direct(int octet)
 {
-    if (octet == 0b10000000)
+    if (octet == (DIR_CODE << 6))
         return (T_DIR);
     return (1);
 }
