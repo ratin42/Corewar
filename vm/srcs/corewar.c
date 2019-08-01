90a3dc7ac8867b261138de8c77d7838d9ccaccbf
Hugo LOMBARD-PLATET <hlombard@e1r8p9.42.fr>
2019-08-01 19:56:10 +0200

    [VM] -d option is ok, and re organised parsing

--- vm/srcs/corewar.c
+++ vm/srcs/corewar.c
@@ -2,13 +2,11 @@
 
 //Connaitre le size min d'un champion pour rejeter comme zaz;
 
-void	corewar_quit(char *str)
+void	init_datas(t_corewar *cor)
 {
-	ft_putstr_fd(str, 2);
-	ft_putchar_fd('\n', 2);
-	exit(EXIT_SUCCESS);
+	ft_bzero(cor, sizeof(t_corewar));
+	cor->n_dump = -1;
 }
-
 
 int main(int ac, char **av)
 {
@@ -18,14 +16,10 @@ int main(int ac, char **av)
 		return (0);
 	else
 	{
-		//INIT ET BZERO;
-		if (get_infos(ac, av, &cor) == -1)
-		{
-			if (DEBUG)
-				ft_printf("\nERROR get_infos == -1\n");
-			return (0);
-		}
+		init_datas(&cor);
+		parse_arguments(ac, av, &cor);
 		create_arena(&cor);
+		// print_arena_state(&cor);
 
 	}
 	return (1);
@@ -34,34 +28,16 @@ int main(int ac, char **av)
 void	create_arena(t_corewar *cor)
 {
 	int i;
-	int p;
-	int j = 0;
 
 	i = -1;
+
 	ft_bzero(cor->arena, MEM_SIZE);
 	while (++i < cor->nb_players)
 	{
+		cor->process[i].alive = 1;
+
+
 		ft_memcpy((void*)&cor->arena + ((MEM_SIZE / cor->nb_players) * i),
 				cor->process[i].code, cor->process[i].size);
 	}
-	
-	p = 0;	
-	i = -1;
-
-	while (++i < MEM_SIZE)
-	{
-		if (j == 64 || i == 0)
-		{
-			ft_putchar('\n');
-			ft_printf("0x%.4x : ", p);
-			p += 64;
-			j = 0;
-		}
-		ft_printf("%.2x ", cor->arena[i]);
-		j++;
-
-	}
-	ft_printf("\n");
-
-
-}
+}	
--- vm/srcs/debug_tools.c
+++ vm/srcs/debug_tools.c
@@ -12,7 +12,7 @@ void	print_process_data(t_corewar *cor, int player_nb)
 	ft_printf("\t=>comment   = [%s]\n", cor->process[player_nb].comment);
 	ft_printf("\t=>prog_size = [%u]\n", cor->process[player_nb].size);
 	ft_printf("\t=>magic     = [%u]\n", cor->process[player_nb].magic);
-	
+
 	ft_printf("\t=>Code:     =\n\n");
 	while (++i < cor->process[player_nb].size)
 	{
@@ -24,4 +24,41 @@ void	print_process_data(t_corewar *cor, int player_nb)
 
 	ft_printf("\n-------------------------------------------------------");
 	ft_printf("\n\n\n");
+}
+
+void	print_arena_state(t_corewar *cor)
+{
+	int p;
+	int j;
+	int i;
+
+	j = 0;
+	p = 0;
+	i = -1;
+	while (++i < MEM_SIZE)
+	{
+		if (j == 64 || i == 0)
+		{
+			ft_putchar('\n');
+			ft_printf("0x%.4x : ", p);
+			p += 64;
+			j = 0;
+		}
+		ft_printf("%.2x ", cor->arena[i]);
+		j++;
+
+	}
+	ft_printf("\n");
+}
+
+void	corewar_usage(void)
+{
+	ft_printf("USAGE:\n");
+}
+
+void	corewar_quit(char *str)
+{
+	ft_putstr_fd(str, 2);
+	ft_putchar_fd('\n', 2);
+	exit(EXIT_SUCCESS);
 }
