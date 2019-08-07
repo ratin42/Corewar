671421d8544752115c8a924f6c2a2c97c7377dc1
Syzhang16 <syzhang@student.42.fr>
2019-08-07 15:37:26 +0200

    started zjmp

--- vm/srcs/instructions/inst_zjmp.c
+++ vm/srcs/instructions/inst_zjmp.c
@@ -2,6 +2,14 @@
 
 void	inst_zjmp(t_corewar *cor, int i)
 {
-	(void)cor;
+	// (void)cor;
 	ft_printf("process[%d] : ZJMP\n", i);
+	int pc;
+
+	if (cor->process[i].carry == 1)
+	{
+		pc = (cor->process[i].pc + 2) % MEM_SIZE;
+		//PC = ( PC + ( param % IDXMOD ) ) % MEMSIZE
+		cor->process[i].pc = pc;
+	}
 }
