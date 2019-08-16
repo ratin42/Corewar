4675eee74151c1f6980c33303fc9b1e9a30f55a4
Raphael TIN <ratin@e1r8p21.42.fr>
2019-08-16 16:10:08 +0200

    [DISASSEMBLER] removed some fonctions

--- disassembler/srcs/parsing/create_list.c
+++ disassembler/srcs/parsing/create_list.c
@@ -6,7 +6,7 @@
 /*   By: ratin <ratin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 17:07:03 by ratin             #+#    #+#             */
-/*   Updated: 2019/08/16 15:45:02 by ratin            ###   ########.fr       */
+/*   Updated: 2019/08/16 15:50:48 by ratin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,26 +37,6 @@ t_op	g_op_tab[17] =
 	{"lfork", 1, {T_DIR, 0, 0}, 15, 1000, "long fork", 0, 1},
 	{"aff", 1, {T_REG, 0, 0}, 16, 2, "aff", 1, 0},
 	{0, 0, {0, 0, 0}, 0, 0, 0, 0, 0}
-};
-
-void (*g_func[17])(t_corewar *cor) =
-{
-	inst_live,
-	NULL,//inst_ld,
-	NULL,//inst_st,
-	NULL,//inst_add,
-	NULL,//inst_sub,
-	inst_and,
-	NULL,//inst_or,
-	NULL,//inst_xor,
-	inst_zjmp,
-	NULL,//inst_ldi,
-	inst_sti,
-	NULL,//inst_fork,
-	NULL,//inst_lld,
-	NULL,//inst_lldi,
-	NULL,//inst_lfork,
-	NULL,//inst_aff
 };
 
 void		create_list(t_corewar *cor)
