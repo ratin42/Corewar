0baeae995343a0c2ddb90b600d7ba79fbfa267ce
Goki LY <gly@student.42.fr>
2019-08-26 16:28:16 +0200

    [VM] Correct functions in debug/ and options/ to pass the norm.

--- vm/srcs/debug/ft_print_process.c
+++ vm/srcs/debug/ft_print_process.c
@@ -5,11 +5,11 @@ void	ft_print_debug(t_plst *plst, char *op_name, int end)
 	if (DEBUG)
 	{
 		if (end == 0)
-			printf("##################  %s  ##################\n", op_name);
-		printf("%s\n", end ? "\nAFTER\n" : "BEFORE\n");
+			ft_printf("##################  %s  ##################\n", op_name);
+		ft_printf("%s\n", end ? "\nAFTER\n" : "BEFORE\n");
 		ft_print_process(plst->p);
 		if (end == 1)
-			printf("##################  END  ##################\n");
+			ft_printf("##################  END  ##################\n");
 	}
 }
 
@@ -17,11 +17,12 @@ void	ft_print_args(t_arg arg)
 {
 	int	i;
 
-	printf("Arg number: %d\n", arg.nb_arg);
+	ft_printf("Arg number: %d\n", arg.nb_arg);
 	i = 0;
 	while (i < arg.nb_arg)
 	{
-		printf("Arg %d: size: [%u], value: [%d]\n", i, arg.size[i], arg.value[i]);
+		ft_printf("Arg %d: size: [%u], value: [%d]\n",
+				i, arg.size[i], arg.value[i]);
 		i++;
 	}
 }
@@ -31,19 +32,19 @@ void	ft_print_process(t_process p)
 	int		i;
 
 	i = 1;
-	printf("Name: %-*s\n", PROG_NAME_LENGTH, p.name);
-	printf("ID: %d\n", p.id);
-	printf("PC: %u\n", p.pc);
-	printf("OG_PC: %u\n", p.og_pc);
-	printf("Carry: %u\n", p.carry);
-	printf("live: %u\n", p.live);
-	printf("No_live: %d\n", p.no_live);
-	printf("Wait: %d\n", p.wait);
-	printf("Op code: %d\n", p.opcode);
-	printf("REG\n");
+	ft_printf("Name: %-*s\n", PROG_NAME_LENGTH, p.name);
+	ft_printf("ID: %d\n", p.id);
+	ft_printf("PC: %u\n", p.pc);
+	ft_printf("OG_PC: %u\n", p.og_pc);
+	ft_printf("Carry: %u\n", p.carry);
+	ft_printf("live: %u\n", p.live);
+	ft_printf("No_live: %d\n", p.no_live);
+	ft_printf("Wait: %d\n", p.wait);
+	ft_printf("Op code: %d\n", p.opcode);
+	ft_printf("REG\n");
 	while (i <= REG_NUMBER)
 	{
-		printf("[%d]: %d\n", i, p.reg[i]);
+		ft_printf("[%d]: %d\n", i, p.reg[i]);
 		i++;
 	}
 }
