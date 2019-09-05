f73209f7960cb7b961330685b9f884c105563ef1
Hugo LOMBARD-PLATET <hlombard@e1r1p13.42.fr>
2019-09-05 13:38:10 +0200

    fixed crash asm non existing file

--- disasm/srcs/parsing/read_process1.c
+++ disasm/srcs/parsing/read_process1.c
@@ -6,7 +6,7 @@
 /*   By: ratin <ratin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/01 20:22:44 by ratin             #+#    #+#             */
-/*   Updated: 2019/09/05 02:09:02 by ratin            ###   ########.fr       */
+/*   Updated: 2019/09/05 13:33:15 by hlombard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	read_process(char *name, t_corewar *cor)
 	int			fd;
 
 	if ((fd = open(name, O_RDONLY)) < 0)
-		decomp_quit(cor, "Open error");
+		decomp_quit(cor, "Open error\n");
 	if ((read(fd, &header, sizeof(t_header)) < 0))
-		decomp_quit(cor, "Read error");
+		decomp_quit(cor, "Read error\n");
 	stock_process_name(cor, &header);
 	stock_process_size(cor, &header);
 	stock_process_comment(cor, &header);
